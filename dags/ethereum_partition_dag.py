20b61055c05002022b68edb77c02e597c4b20467
Samag Chakrabarti <samagchakrabarti@gmail.com>
2026-03-16 10:34:27 +0530

    Updated ethereum partition DAG configuration

--- dags/ethereum_partition_dag.py
+++ dags/ethereum_partition_dag.py
@@ -1,22 +1,20 @@
 from __future__ import print_function
-
 import logging
 
 from airflow.models import Variable
-
 from ethereumetl_airflow.build_partition_dag import build_partition_dag
 
-logging.basicConfig()
-logging.getLogger().setLevel(logging.DEBUG)
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
-# airflow DAG
+# Build Airflow DAG for Ethereum partitioning
 DAG = build_partition_dag(
-    dag_id='ethereum_partition_dag',
-    partitioned_project_id='blockchain-etl-internal',
-    partitioned_dataset_name = 'crypto_ethereum_partitioned',
-    public_project_id = 'bigquery-public-data',
-    public_dataset_name = 'crypto_ethereum',
-    load_dag_id='ethereum_load_dag',
-    notification_emails=Variable.get('notification_emails', None),
-    schedule_interval='30 9 * * *',
+    dag_id="ethereum_partition_dag",
+    partitioned_project_id="blockchain-etl-internal",
+    partitioned_dataset_name="crypto_ethereum_partitioned",
+    public_project_id="bigquery-public-data",
+    public_dataset_name="crypto_ethereum",
+    load_dag_id="ethereum_load_dag",
+    notification_emails=Variable.get("notification_emails", default_var=None),
+    schedule_interval="30 9 * * *"
 )
