251b13533604184581955cefe2f8b0afb0d624ff
Samag Chakrabarti <samagchakrabarti@gmail.com>
2026-03-17 10:21:59 +0530

    Refactored ethereum verify streaming DAG

--- dags/ethereum_verify_streaming_dag.py
+++ dags/ethereum_verify_streaming_dag.py
@@ -1,19 +1,19 @@
 from __future__ import print_function
-
 import logging
 
 from ethereumetl_airflow.build_verify_streaming_dag import build_verify_streaming_dag
 from ethereumetl_airflow.variables import read_verify_streaming_dag_vars
 
-logging.basicConfig()
-logging.getLogger().setLevel(logging.DEBUG)
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
-# When searching for DAGs, Airflow will only consider files where the string "airflow" and "DAG" both appear in the
-# contents of the .py file.
+# Airflow detects DAGs only if both "airflow" and "DAG" appear in the file
+dag_config = read_verify_streaming_dag_vars(
+    var_prefix="ethereum_",
+    max_lag_in_minutes=10
+)
+
 DAG = build_verify_streaming_dag(
-    dag_id='ethereum_verify_streaming_dag',
-    **read_verify_streaming_dag_vars(
-        var_prefix='ethereum_',
-        max_lag_in_minutes=10,
-    )
+    dag_id="ethereum_verify_streaming_dag",
+    **dag_config
 )
